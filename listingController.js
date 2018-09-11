24cbccd17e46b875d81f91912cb180c700bb7428
aambrioso01 <40615096+aambrioso01@users.noreply.github.com>
2018-09-10 23:12:55 -0400

    Assignment 2
    All files, first draft

--- listingController.js
+++ listingController.js
@@ -7,8 +7,21 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+      $scope.listings.push({"code":$scope.code, "name":$scope.name, "coordinates":{"latitude":$scope.latitude, "longitude":$scope.longitude}, "address":$scope.address});
+      $scope.code="";
+      $scope.name="";
+      $scope.coordinates.latitude="";
+      $scope.coordinates.longitude="";
+      $scope.address="";
+    };
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice(index, 1 );
+    };
+    $scope.showDetails = function(index) {
+      document.getElementById("lat").innerHTML = $scope.listings[index].coordinates.latitude;
+      document.getElementById("long").innerHTML = $scope.listings[index].coordinates.longitude;
+      document.getElementById("add").innerHTML = $scope.listings[index].address;
+    };
   }
 ]);
